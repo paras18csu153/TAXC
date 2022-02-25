c9cce918d59ae483d6ae51990b86c6b65a2ebe07
paras18csu153 <paras18csu153@gmail.com>
2022-02-25 22:57:19 +0530

    Removed Console Message

--- users/controllers/user.controller.js
+++ users/controllers/user.controller.js
@@ -412,7 +412,6 @@ exports.changePassword = async (req, res) => {
             });
         }
     } catch (err) {
-        console.log(err);
         return res.status(500).send({
             message: 'Internal Server Error.'
         });
