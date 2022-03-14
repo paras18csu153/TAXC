329e18b42fcb44973d93b97fb49d1ae1d3e214b3
paras18csu153 <paras18csu153@gmail.com>
2022-03-14 20:40:41 +0530

    Trips Microservice Completed.

--- trips/app.js
+++ trips/app.js
@@ -12,6 +12,7 @@ db();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var tripsRouter = require('./routes/trip.route');
 
 var app = express();
 
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/trips', tripsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
